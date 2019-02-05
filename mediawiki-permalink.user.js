acdd898ff5f83d10532a90cd1cbdabbef976bb9c
fox0 <red-fox0@yandex.ru>
2019-02-05 12:46:50 +0700

    Update mediawiki-permalink.user.js

--- mediawiki-permalink.user.js
+++ mediawiki-permalink.user.js
@@ -3,12 +3,12 @@
 // @version 4
 // @match https://*.wikipedia.org/w*
 // @match https://*.wiktionary.org/w*
+// @match https://commons.wikimedia.org/w*
 // @match https://*.wikifur.com/w*
 // @match https://posmotre.li/*
 // @match https://lurkmore.to/*
 // @match http://xgu.ru/w*
 // ==/UserScript==
-
 (function(){
   var link = document.getElementById('t-permalink').firstChild;
   link.search = link.search.replace(/(\?)title=.*&(oldid=\d+)/, '$1$2');
