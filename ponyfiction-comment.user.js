d021aca9cf504bd155f1378552a7a817853940d0
fox0 <red-fox0@yandex.ru>
2019-02-08 21:28:58 +0700

    Update ponyfiction-comment.user.js

--- ponyfiction-comment.user.js
+++ ponyfiction-comment.user.js
@@ -1,30 +1,29 @@
 // ==UserScript==
-// @name     ponyfiction comment
-// @version  1
-// @match https://ponyfiction.org/story/*
+// @name ponyfiction comment
+// @version 2
+// @match https://ponyfiction.org/*
 // ==/UserScript==
+(function() {
+  function init() {
+    console.log('userscript');
+    Array.from(document.getElementsByClassName('comment-by-topic-author')).forEach(function(e) {
+      e.parentElement.style = 'background-color: #cfc;';
+    });
+    Array.from(document.getElementsByClassName('vote-total')).forEach(function(e) {
+      var total = parseInt(/(\d+)/.exec(e.title)[1]);
+      var vote = parseInt(e.innerHTML);
+      //u - d = v
+      //u + d = t
+      var upvotes = (total + vote) / 2;
+      var downvotes = total - upvotes;
 
-//todo прицепиться к событию Reload ajax
-(function(){
-  var ls;
-  
-  ls = document.getElementsByClassName('comment-by-topic-author');
-  Array.from(ls).forEach(function(e) {
-  	e.parentElement.style = 'background-color: #cfc;';
-	});
-  
-  ls = document.getElementsByClassName('vote-total');
-  Array.from(ls).forEach(function(e) {
-    var total = parseInt(/(\d+)/.exec(e.title)[1]);
-    var vote = parseInt(e.innerHTML);
-    //u - d = v
-    //u + d = t
-    var upvotes = (total + vote) / 2;
-    var downvotes = total - upvotes;
-
-    var r = '';
-    r += '<span style="color: #' + (upvotes>0?'0a0':'888') + ';">+' + upvotes + '</span> ';
-    r += '<span style="color: #' + (downvotes>0?'a00':'888') + ';">-' + downvotes + '</span>';
-    e.innerHTML = r;
-	});
+      var r = '';
+      r += '<span style="color: #' + (upvotes>0?'0a0':'888') + ';">+' + upvotes + '</span> ';
+      r += '<span style="color: #' + (downvotes>0?'a00':'888') + ';">-' + downvotes + '</span>';
+      e.innerHTML = r;
+    });  
+  }
+  //todo пагинация комментариев не работает
+  document.addEventListener('amajaxify:endrequest', init);
+  init();
 })();
