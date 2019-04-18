3d4c281cb222c74d03d69f5ff989895cc7ca882e
fox0 <red-fox0@yandex.ru>
2019-04-18 18:10:35 +0700

    Create anilibria-fix-adblock.user.cs

---
+++ anilibria-fix-adblock.user.cs
@@ -0,0 +1,13 @@
+/* ==UserStyle==
+@name           anilibria fix adblock
+@namespace      github.com/openstyles/stylus
+@version        0.0.1
+==/UserStyle== */
+@-moz-document domain("anilibria.tv") {
+	ul.main-navigation a {
+		padding: 14px 0;
+	}
+	.footer {
+		height: 140px;
+	}
+}
