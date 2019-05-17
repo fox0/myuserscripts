ea4c966284ff81e0b3b7d71af3bd91c840b2e6b3
fox0 <red-fox0@yandex.ru>
2019-05-17 13:00:54 +0700

    Update anilibria.user.js

--- anilibria.user.js
+++ anilibria.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name anilibria waiter
-// @version 2
+// @version 3
 // @match https://www.anilibria.tv/release/*
 // ==/UserScript==
 (function(){
-  var el = document.querySelector('[id^="torrentTableInfo"]');
-  var r = el.innerText.replace(/.*\s1-(\d+)\s.*/, '$1');
+  var el = document.querySelector('[id^="torrentTableDate"]');
+  var r = el.innerText.replace(/.*\s(\d{2}\.\d{2}\.\d{4})\s.*/, '$1');
   document.getElementsByTagName('title')[0].innerHTML = r;
   
   setTimeout(location.reload, 1000 * 60 * 5); //min
