4b420ea7db18b890c3f648f864d02252fb633c2a
fox0 <red-fox0@yandex.ru>
2019-05-17 12:56:36 +0700

    +qarchive.ru

--- qaru.site.user.js
+++ qaru.site.user.js
@@ -6,7 +6,7 @@
 (function() {
   var ls = document.getElementsByTagName('cite');
   Array.from(ls).forEach(function(e) {
-    if (e.innerHTML.includes('qaru.site/')) {
+    if (e.innerHTML.includes('qaru.site/') || e.innerHTML.includes('qarchive.ru/')) {
       e.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style = 'display: none;'; 
     }
   });
