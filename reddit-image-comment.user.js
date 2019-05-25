376b9d1479ba81f3a8d76ba6b07d24af8efddc8e
fox0 <red-fox0@yandex.ru>
2019-05-26 00:11:24 +0700

    Update reddit-image-comment.user.js

--- reddit-image-comment.user.js
+++ reddit-image-comment.user.js
@@ -1,21 +1,30 @@
 // ==UserScript==
 // @name reddit image comment
-// @version 1
-// @match https://www.reddit.com/r/Pikabu/comments/*
+// @version 2
+// @match https://www.reddit.com/r/*/comments/*
 // ==/UserScript==
 (function() {
+  var count_qs = 0;
+  
   function _update() {
-  	var qs = document.querySelectorAll('div[data-test-id="comment"]');
-  	Array.from(qs).forEach(function(e) {
-      //e.style = 'border: solid #0f0 1px;'
-      var text = e.lastElementChild.innerHTML;
-      
-      text = text.replace(/(href="(.*\.jpg)".*<\/a>)/, '$1<p><img src="$2" style="max-width:100%;"><\p>');
-      text = text.replace(/(href="(https:\/\/i.imgur.com\/.*\.gif)v".*<\/a>)/, '$1<p><img src="$2" style="max-width:100%;"><\p>');
-      
-      e.lastElementChild.innerHTML = text;
+  	var qs = document.querySelectorAll('div[data-test-id="comment"] a');
+    if (count_qs == qs.length) {
+      return;  
+    }
+    count_qs = qs.length;
+  	Array.from(qs).forEach(function(a) {
+      if (a.href.endsWith('.jpg') || a.href.endsWith('.png')) {
+        var img = '<img src="' + a.href + '" style="max-width:100%;">';
+        if (a.innerHTML == a.href) {
+          a.innerHTML = img;
+        } else {
+          a.innerHTML += '<br>' + img;
+        }
+        a.href = 'http://localhost';
+      }
+      //text = text.replace(/(href="(https:\/\/i.imgur.com\/.*\.gif)v".*<\/a>)/, '$1<p><img src="$2" style="max-width:100%;"><\p>');
   	});
   }
+  window.onscroll = _update;
   _update();
-  
 })();
