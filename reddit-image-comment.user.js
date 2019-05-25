9e9edfad71e9f4a021c7e74a6f03ce02cd006a7d
fox0 <red-fox0@yandex.ru>
2019-05-26 01:00:06 +0700

    Update reddit-image-comment.user.js

--- reddit-image-comment.user.js
+++ reddit-image-comment.user.js
@@ -1,29 +1,35 @@
 // ==UserScript==
 // @name reddit image comment
-// @version 2
+// @version 3
 // @match https://www.reddit.com/r/*/comments/*
 // ==/UserScript==
 (function() {
   var count_qs = 0;
   
   function _update() {
-  	var qs = document.querySelectorAll('div[data-test-id="comment"] a');
+    var qs = document.querySelectorAll('div[data-test-id="comment"] a');//todo a[data]^
     if (count_qs == qs.length) {
       return;  
     }
     count_qs = qs.length;
-  	Array.from(qs).forEach(function(a) {
+    Array.from(qs).forEach(function(a) {
+      var text = null;
       if (a.href.endsWith('.jpg') || a.href.endsWith('.png')) {
-        var img = '<img src="' + a.href + '" style="max-width:100%;">';
-        if (a.innerHTML == a.href) {
-          a.innerHTML = img;
-        } else {
-          a.innerHTML += '<br>' + img;
-        }
-        a.href = 'http://localhost';
+        text = '<img src="' + a.href + '" style="max-width:100%;">';
       }
-      //text = text.replace(/(href="(https:\/\/i.imgur.com\/.*\.gif)v".*<\/a>)/, '$1<p><img src="$2" style="max-width:100%;"><\p>');
-  	});
+      if (a.href.includes('format=mp4')) {
+        text = '<video controls autoplay><source src="' + a.href + '"></video>';
+      }
+      if (text) {
+        var p = document.createElement('p');
+        if (a.innerHTML == a.href) {
+          p.innerHTML = text;
+        } else {
+          p.innerHTML = a.innerHTML + '<br>' + text;
+        }
+        a.parentNode.replaceChild(p, a);
+      }
+    });
   }
   window.onscroll = _update;
   _update();
