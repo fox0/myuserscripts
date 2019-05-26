13dc7eb4bdfd213e24eff31eebe592e549640fcb
fox0 <red-fox0@yandex.ru>
2019-05-26 17:42:59 +0700

    Update hp.user.js

--- hp.user.js
+++ hp.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name PH downloader
-// @version 4
+// @version 5
 // @match https://*.pornhub.com/view_video.php*
 // ==/UserScript==
 (function() {
   console.log('[userscript] PH downloader');
   
   function userscript_add_link() {
-    var url = document.getElementsByTagName('video')[0].children[0].src;
-    if (url.includes('view_video.php')) {
+    var qs = document.getElementsByTagName('video');
+    if (qs.length == 0) {
       setTimeout(userscript_add_link, 500); //try again
       return;
     }
+    var url = qs[0].children[0].src;
     var d = document.createElement('div');
     d.className = 'tab-menu-wrapper-cell';
     d.innerHTML = '<a href="' + url + '" target="_blank" style="font-size:16px;">Скачать (direct)</a>';
