289acf1fdcc9a471d783e725355a4237f920afb4
fox0 <red-fox0@yandex.ru>
2019-05-26 17:57:22 +0700

    Update hp.user.js

--- hp.user.js
+++ hp.user.js
@@ -1,27 +1,37 @@
 // ==UserScript==
 // @name PH downloader
-// @version 5
+// @version 6
 // @match https://*.pornhub.com/view_video.php*
 // ==/UserScript==
 (function() {
-  console.log('[userscript] PH downloader');
+  function main() {
+    try {
+			_main();
+    } catch (e) {
+    	console.log('[userscript ph] ERROR: ' + e);
+		}
+  }
   
-  function userscript_add_link() {
+  function _main() {
     var qs = document.getElementsByTagName('video');
-    if (qs.length == 0) {
-      setTimeout(userscript_add_link, 500); //try again
+    if (qs.length == 0){
+      setTimeout(main, 500); //try again
       return;
     }
     var url = qs[0].children[0].src;
+    if (url.includes('view_video.php')) {
+      setTimeout(main, 500); //try again
+      return;
+    }
     var d = document.createElement('div');
     d.className = 'tab-menu-wrapper-cell';
     d.innerHTML = '<a href="' + url + '" target="_blank" style="font-size:16px;">Скачать (direct)</a>';
     document.getElementsByClassName('tab-menu-wrapper-row')[0].appendChild(d); 
   }
-  
+
   window.onload = function() {
     document.querySelector('[data-tab=download-tab]').style = 'display:none;';
-	  document.querySelector('[data-tab=add-to-tab]').style = 'display:none;';
-    userscript_add_link();
+    document.querySelector('[data-tab=add-to-tab]').style = 'display:none;';
+    main();
   };
 }());
