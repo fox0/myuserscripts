35d49acd6878f0f1925ef7d2a780c202cdbb5e90
fox0 <red-fox0@yandex.ru>
2019-05-29 21:49:31 +0700

    Update ficbook-not-js.user.js

--- ficbook-not-js.user.js
+++ ficbook-not-js.user.js
@@ -1,11 +1,6 @@
 // ==UserScript==
 // @name ficbook fucking js
-// @version 1
+// @version 2
 // @match https://ficbook.net/*
 // ==/UserScript==
-(function() {
-  var ls = document.getElementsByClassName('urlize');
-  Array.from(ls).forEach(function(e) {
-    e.classList.remove('urlize');
-  });
-}());
+for (const e of document.getElementsByClassName('urlize')) e.classList.remove('urlize');
