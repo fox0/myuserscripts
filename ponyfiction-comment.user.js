22d65b56c9e9a70fa09cd42093429dd6b1d95fdd
fox0 <red-fox0@yandex.ru>
2019-05-29 22:11:34 +0700

    Update ponyfiction-comment.user.js

--- ponyfiction-comment.user.js
+++ ponyfiction-comment.user.js
@@ -1,29 +1,21 @@
 // ==UserScript==
 // @name ponyfiction comment
-// @version 2
+// @version 3
 // @match https://ponyfiction.org/*
 // ==/UserScript==
-(function() {
-  function init() {
-    console.log('userscript');
-    Array.from(document.getElementsByClassName('comment-by-topic-author')).forEach(function(e) {
-      e.parentElement.style = 'background-color: #cfc;';
-    });
-    Array.from(document.getElementsByClassName('vote-total')).forEach(function(e) {
-      var total = parseInt(/(\d+)/.exec(e.title)[1]);
-      var vote = parseInt(e.innerHTML);
-      //u - d = v
-      //u + d = t
-      var upvotes = (total + vote) / 2;
-      var downvotes = total - upvotes;
+for (const e of document.getElementsByClassName('comment-by-topic-author')) {
+  e.parentElement.style = 'background-color: #cfc;';
+}
 
-      var r = '';
-      r += '<span style="color: #' + (upvotes>0?'0a0':'888') + ';">+' + upvotes + '</span> ';
-      r += '<span style="color: #' + (downvotes>0?'a00':'888') + ';">-' + downvotes + '</span>';
-      e.innerHTML = r;
-    });  
-  }
-  //todo пагинация комментариев не работает
-  document.addEventListener('amajaxify:endrequest', init);
-  init();
-})();
+for (const e of document.getElementsByClassName('vote-total')) {
+  var total = parseInt(/(\d+)/.exec(e.title)[1]);
+  var vote = parseInt(e.innerHTML);
+  //u - d = v
+  //u + d = t
+  var upvotes = (total + vote) / 2;
+  var downvotes = total - upvotes;
+
+  e.innerHTML = 
+    '<span style="color: #' + (upvotes>0?'0a0':'888') + ';">+' + upvotes + '</span> ' +
+    '<span style="color: #' + (downvotes>0?'a00':'888') + ';">-' + downvotes + '</span>';
+});  
