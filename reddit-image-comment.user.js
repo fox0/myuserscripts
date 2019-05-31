acf009b4b0bfa147008e7815e0875052e2a15cf0
fox0 <red-fox0@yandex.ru>
2019-05-31 21:04:07 +0700

    Update reddit-image-comment.user.js

--- reddit-image-comment.user.js
+++ reddit-image-comment.user.js
@@ -1,10 +1,28 @@
 // ==UserScript==
 // @name reddit image comment
-// @version 5
+// @version 0.6
 // @match https://www.reddit.com/r/*/comments/*
 // ==/UserScript==
 (function() {
   var count_qs = 0;
+  
+  function http_get(url) {
+    var x = new XMLHttpRequest();
+    x.open('GET', url, false);
+    x.send(null);
+    return x.responseText;
+  }
+  
+  function http_get_async(url, callback) {
+    var x = new XMLHttpRequest();
+    x.onreadystatechange = function() { 
+      if (x.readyState == 4 && x.status == 200) {
+        callback(x.responseText);
+      }
+    };
+    x.open('GET', url, true);
+    x.send(null);
+  }
   
   function _update() {
     var qs = document.querySelectorAll('div[data-test-id="comment"] a');//todo a[data]^
@@ -13,12 +31,15 @@
     }
     count_qs = qs.length;
     for (const a of qs) {
+      var url = a.href;
       var text = null;
-      if (a.href.endsWith('.jpg') || a.href.endsWith('.png') || a.href.endsWith('.gif')) {
-        text = '<img src="' + a.href + '" style="max-width:100%;">';
-      }
-      if (a.href.includes('format=mp4')) {
-        text = '<video controls autoplay><source src="' + a.href + '"></video>';
+      if (url.startsWith('https://imgur.com/')) {
+        url = http_get(url).match(/<link rel="image_src" href="(.*)"/)[1];//todo async
+      }      
+      if (url.endsWith('.jpg') || url.endsWith('.png') || url.endsWith('.gif')) {
+        text = '<img src="' + url + '" style="max-width:100%;">';
+      } else if (url.includes('format=mp4')) {
+        text = '<video controls autoplay><source src="' + url + '"></video>';
       }
       if (text) {
         var p = document.createElement('p');
