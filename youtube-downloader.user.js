19d4c68143d8e596aa14c620ac3daa1bbd42a757
fox0 <red-fox0@yandex.ru>
2019-06-22 21:31:28 +0700

    +onlinevideoconverter

--- youtube-downloader.user.js
+++ youtube-downloader.user.js
@@ -1,60 +1,72 @@
 // ==UserScript==
 // @name Youtube downloader
-// @version 2
+// @version 0.3
 // @match https://www.youtube.com/watch*
 // @match http://convert2mp3.net/en/index.php*
+// @match https://www.onlinevideoconverter.com/ru/youtube-converter*
+// @match https://www.onlinevideoconverter.com/ru/success
 // ==/UserScript==
-
 (function() {
-  function onload_you() {
-    var a = document.createElement('a');
-    a.target = '_blank';
-    a.style = 'display: inline-block; background: #222; color: #ddd; text-align: center; padding: 5px; margin:10px; font-size: 1.5rem; text-decoration: none;';
-    
-    a.innerText = 'Скачать mp4';
-    a.href = 'http://convert2mp3.net/en/index.php#mp4' + document.URL;
-    //onclick потому что document.URL может смениться без перезагрузки страницы
-    a.onclick = function() {
-      this.href = 'http://convert2mp3.net/en/index.php#mp4' + document.URL;
-      return true;
-    }
-    document.getElementById('messages').appendChild(a);
-    
-    var a2 = a.cloneNode();
-    a2.innerText = 'Скачать aac';
-    a2.href = 'http://convert2mp3.net/en/index.php#aac' + document.URL;
-    a2.onclick = function() {
-      this.href = 'http://convert2mp3.net/en/index.php#aac' + document.URL;
-      return true;
-    }
-    document.getElementById('messages').appendChild(a2);
-  }
-  
-  function onload_dw_mp4() {
-    var url = window.location.hash.substring(4);
-    document.getElementById('urlinput').value = url;
-    document.getElementById('select_main').value = 'mp4';
-    document.getElementById('convertForm').submit(); 
-  }
-  
-  function onload_dw_aac() {
-    var url = window.location.hash.substring(4);
-    document.getElementById('urlinput').value = url;
-    document.getElementById('select_main').value = 'aac';
-    document.getElementById('convertForm').submit(); 
-  }
-  
-  function onload_dw2() {
-    document.getElementsByClassName('btn-success')[0].click();
-  }
-  
   if (document.URL.startsWith('https://www.youtube.com/watch')) {
-    window.onload = onload_you;
+    window.onload = function() {
+      var a;
+      a = get_a('http://convert2mp3.net/en/index.php#mp4', 'mp4 (convert2mp3.net)');
+      document.getElementById('messages').appendChild(a);
+      a = get_a('http://convert2mp3.net/en/index.php#aac', 'aac (convert2mp3.net)');
+      document.getElementById('messages').appendChild(a);
+      a = get_a('https://www.onlinevideoconverter.com/ru/youtube-converter#mp4', 'mp4 (onlinevideoconverter.com)');
+      document.getElementById('messages').appendChild(a);
+      
+      function get_a(url, text) {
+        var r;
+        r = document.createElement('a');
+        r.style = 
+          'display: inline-block;' + 
+          'background: #222;' + 
+          'color: #ddd;' + 
+          'text-align: center;' + 
+          'padding: 5px;' + 
+          'margin:10px;' +
+          'font-size: 1.5rem;' +
+          'text-decoration: none;';
+        r.target = '_blank';
+        r.innerText = text;
+        r.href = url + document.URL;
+        //потому что document.URL может смениться без перезагрузки страницы
+        r.onclick = function() {
+          this.href = url + document.URL;
+          return true;
+        }
+        return r;
+      }
+    };
   } else if (document.URL.startsWith('http://convert2mp3.net/en/index.php#mp4')) {
-    window.onload = onload_dw_mp4;
+    window.onload = function() {
+      var url = window.location.hash.substring(4);
+    	document.getElementById('urlinput').value = url;
+    	document.getElementById('select_main').value = 'mp4';
+    	document.getElementById('convertForm').submit(); 
+    };
   } else if (document.URL.startsWith('http://convert2mp3.net/en/index.php#aac')) {
-    window.onload = onload_dw_aac;
+    window.onload = function() {
+      var url = window.location.hash.substring(4);
+      document.getElementById('urlinput').value = url;
+      document.getElementById('select_main').value = 'aac';
+      document.getElementById('convertForm').submit(); 
+    };
   } else if (document.URL.startsWith('http://convert2mp3.net/en/index.php?p=complete')) {
-    window.onload = onload_dw2;
+    window.onload = function() {
+			document.getElementsByClassName('btn-success')[0].click();
+    };
+  } else if (document.URL.startsWith('https://www.onlinevideoconverter.com/ru/youtube-converter')) {
+    window.onload = function() {
+      var url = window.location.hash.substring(4);
+   	 	document.getElementById('texturl').value = url;
+    	document.getElementById('convert1').click();
+    };
+  } else if (document.URL.startsWith('https://www.onlinevideoconverter.com/ru/success')) {
+    window.onload = function() {
+      document.getElementById('downloadq').click();
+    };
   }
 }());
