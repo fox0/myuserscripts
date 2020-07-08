8169539a0054fa8e96c879f301899be375783014
fox0 <red-fox0@yandex.ru>
2020-07-08 16:43:08 +0700

    +overcoder.net

--- google-blacklist.user.js
+++ google-blacklist.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
-// @name Google blacklist
-// @version 3
+// @name google blacklist
+// @version 0.3
 // @match https://www.google.com/search*
 // ==/UserScript==
 (function() {
-  let blacklist = [
-  	'qaru.site/',
+  let black_list = [
+    'qaru.site/',
     'qarchive.ru/',
+    'overcoder.net/',
     'wiki-org.ru/',
+    'dic.academic.ru/'
   ];
   
   for (const e of document.getElementsByTagName('cite')) {
-    for (const i of blacklist) {
+    for (const i of black_list) {
       if (e.innerHTML.includes(i)) {
         e.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style = 'display: none;';
         break;
