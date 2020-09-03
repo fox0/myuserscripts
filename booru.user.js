0cd82e54fddbe0cb662508d9db47503aad1f5205
fox0 <red-fox0@yandex.ru>
2020-09-03 22:25:51 +0700

    Update booru.user.js

--- booru.user.js
+++ booru.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name     booru big img
-// @version  0.1
+// @version  0.2
 // @match    https://derpibooru.org/search?q=first_seen_at.gt:3%20days%20ago*
-// @match		 https://derpibooru.org/search?*q=first_seen_at.gt%3A3+days+ago
+// @match    https://derpibooru.org/search?*q=first_seen_at.gt%3A3+days+ago
 // ==/UserScript==
 (function(){
   window.onload = function() {
@@ -10,11 +10,12 @@
     var css = 'display:block; max-height: 100% !important;height: 100% !important; max-width: 100% !important; width: 100% !important;';
     var ls = document.getElementsByClassName('image-container');
     for (var i=0; i<ls.length; i++) {
-      var url = JSON.parse(ls[i].dataset.uris)['large'];
-      ls[i].getElementsByTagName('img')[0].src = url;
       ls[i].style.cssText = css;
       ls[i].parentElement.style.cssText = css;
-      ls[i].parentElement.parentElement.style.cssText = css; 
+      ls[i].parentElement.parentElement.style.cssText = css;
+      var url = JSON.parse(ls[i].dataset.uris)['large'];
+      if (url.endsWith('webm')) continue;
+      ls[i].getElementsByTagName('img')[0].src = url;
     }
   }
 })();
