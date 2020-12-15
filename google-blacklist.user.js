f51c205c5199ca3e09e7ddca517d76c99f2078ca
fox0 <red-fox0@yandex.ru>
2020-12-15 17:52:41 +0700

    Update google-blacklist.user.js

--- google-blacklist.user.js
+++ google-blacklist.user.js
@@ -1,21 +1,25 @@
 // ==UserScript==
 // @name google blacklist
-// @version 0.3
+// @version 0.4
 // @match https://www.google.com/search*
 // ==/UserScript==
 (function() {
   let black_list = [
-    'qaru.site/',
-    'qarchive.ru/',
-    'overcoder.net/',
-    'wiki-org.ru/',
-    'dic.academic.ru/'
+    'qaru.site',
+    'qarchive.ru',
+    'overcoder.net',
+    'wiki-org.ru',
+    'coderoad.ru',
+    'www.rupython.com',
+    
+    'dic.academic.ru'
   ];
   
   for (const e of document.getElementsByTagName('cite')) {
+    console.log(e.innerHTML);
     for (const i of black_list) {
       if (e.innerHTML.includes(i)) {
-        e.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style = 'display: none;';
+        e.parentNode.parentNode.parentNode.parentNode.style = 'display: none;';
         break;
       }
     }
