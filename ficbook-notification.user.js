b1307c759a746bf6434607c5f0617760dfc74914
fox0 <red-fox0@yandex.ru>
2022-12-18 15:03:15 +0700

    Create ficbook-notification.user.js

---
+++ ficbook-notification.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         count notification
+// @version      0.3
+// @match        https://ficbook.net/notifications
+// @grant        none
+// ==/UserScript==
+(function() {
+    function reflesh_count() {
+        fetch('/user_notifications/get_new', { method: 'POST'})
+            .then((response) => response.json())
+            .then((json) => {
+                var count = json.data.count;
+                var title = count === 0 ? '-///-' : '[' + count + ']';
+                document.getElementsByTagName('title')[0].innerHTML = title + ' Оповещения';
+        });
+    }
+    setTimeout(reflesh_count, 1000 * 63 * 3); //min
+    reflesh_count();
+})();
