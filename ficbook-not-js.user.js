04110e0e8ed39690d6f547b59350c1bd7714b21e
fox0 <red-fox0@yandex.ru>
2022-12-18 15:06:29 +0700

    Delete ficbook-not-js.user.js

--- ficbook-not-js.user.js
+++
@@ -1,6 +0,0 @@
-// ==UserScript==
-// @name ficbook fucking js
-// @version 2
-// @match https://ficbook.net/*
-// ==/UserScript==
-for (const e of document.getElementsByClassName('urlize')) e.classList.remove('urlize');
